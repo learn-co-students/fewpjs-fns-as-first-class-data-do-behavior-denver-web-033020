f4232c3eb0801cd8095e4b9d81679206b5427e6f
kathryn0908 <kathrynlael@gmail.com>
2020-05-12 15:29:18 -0600

    committing updates

--- index.js
+++ index.js
@@ -3,9 +3,31 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  const timeString = document.getElementById('time').value
-  displayMessage(greet(timeString))
+  const timeString = document.getElementById('time').value;
+  displayMessage(greet(timeString));
 }
+
+// document.getElementById("greeting").addEventListener("submit", greet)
+
+function greet(timeString){
+  let timeInteger = timeString.split(":");
+
+  if (parseInt(timeInteger[0]) < 12){
+    return "Good Morning";
+  }
+  else if (parseInt(timeInteger[0] >= 12) && parseInt(timeInteger[0] < 17)){
+    return "Good Afternoon";
+  }
+  else if (parseInt(timeInteger[0] >= 17)){
+    return "Good Evening";
+  }
+}
+
+function displayMessage(greet){
+  document.getElementById('greeting').innerText = greet;
+}
+
+
 
 /* End Given Code, don't edit above here...*/
 
